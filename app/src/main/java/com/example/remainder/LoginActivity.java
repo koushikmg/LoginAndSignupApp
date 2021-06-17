2207ad3c53bf8c4bc81ffffdf7cb5b663ec93250
koushikmg <koushikmg2028@gmail.com>
2021-06-17 13:04:17 +0530

    Added backend code

--- app/src/main/java/com/example/remainder/LoginActivity.java
+++ app/src/main/java/com/example/remainder/LoginActivity.java
@@ -2,13 +2,71 @@ package com.example.remainder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button login;
+    TextView register;
+    TextInputLayout emailError, passError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        passError = (TextInputLayout) findViewById(R.id.passError);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                SetValidation();
+                Intent intent = getIntent();
+                String name = intent.getStringExtra("name");
+                String email = intent.getStringExtra("email");
+                String phno = intent.getStringExtra("phno");
+                String password = intent.getStringExtra("pass");
+                Boolean validLogin = Validate(email,password);
+                if(validLogin){
+                    Toast.makeText(getApplicationContext(),"Successful Login",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Invalid Credentials",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // redirect to RegisterActivity
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+    private Boolean Validate(String mail, String pass) {
+        Boolean validLogin = false;
+        if(email.getText().toString().equals(mail) && password.getText().toString().equals(pass))
+        {
+            validLogin = true;
+        }
+        return validLogin;
+    }
+
 }
